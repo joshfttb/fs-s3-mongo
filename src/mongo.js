af2eb852e716fb0abfec18879ad6edd15b6a3ded
JoshF <josh@brinkbit.com>
2015-12-11 19:14:24 -0500

    create permissions function

--- src/mongo.js
+++ src/mongo.js
@@ -4,6 +4,117 @@ const utils = require( './utils.js' );
 
 /* eslint no-unused-vars: 0 */
 
+module.exports.verify = new Promise(function(resolve, reject, user, operation, path) {
+
+    var lastParent;
+    var pathArray;
+    var i;
+    var folderFail;
+    var isParent;
+    var fileExists;
+    var fileId;
+    var permissionsArray;
+    var permissions;
+
+    //josh/test/docs/sext.doc
+
+    //first we need to find out how far down the path currently exists
+
+    //make the path into an array
+    //todo: need to remove the first part if we have an '/' up front
+    pathArray= path.split('/');
+
+    // start pathFind as an empty string
+    lastParent = '';
+
+    //iterate down the path,
+    i = 0;
+    folderFail = false;
+    do {
+        //add the next part of the path
+        lastParent += pathArray[i];
+
+        //store the fileid
+        //i=2, lastParent = josh.children.test.children.docs.fileId
+        fileId = lastParent + '.fileId';
+
+        //see if this is a folder
+        if (!File.find({$and: [{_id: fileId}, {type: 'folder'}]}).fetch()) {
+            folderFail = true;
+        }
+
+        //add '.children'
+        //i=2, lastParent = josh.children.test.children.docs.children
+        lastParent +='.children';
+
+        //run the test
+        isParent= Structure.find({[lastParent]: {$exists: true, $ne: null}}).fetch();
+
+        //if there's another level and this was not a folder, we have a problem
+        if (isParent && folderFail) {
+            reject('tried to add object to file');
+        }
+
+        //if there is another level, increment
+        if (isParent) i++;
+
+    } while (isParent);
+
+    //determine if the file exists, eg, is an exact match
+    fileExists = (i === pathArray.length);
+
+    //if this is not an exact match, and the last path was not a folder, we have a problem
+    if (!fileExists && folderFail) {
+        reject('tried to add object to file');
+    }
+
+    //get permissions for the user on the last parent
+    permissionsArray = File.find({_id: id}).fetch();
+    permissionsArray.forEach(function (item) {
+        if (user === item.userId) {
+            permissions = item.permissions
+        }
+    });
+
+    //split permissions into an array
+    permissions = permissions.split(',');
+
+    //we now know where our path ends and what our user's permissions are on that end. time to test things
+    //important vars:
+    // fileExists
+    // permissions
+
+    //a file that already exists is required for some operations, and excluded for others
+    //required for read, update, destroy
+    if(!fileExists && operation === 'read' || 'update' || 'destroy') {
+        reject('object does not exist');
+    }
+    //can't exist for write
+    if(fileExists && operation === 'write') {
+        reject('object already exists at that path');
+    }
+
+    //test permissions against various actions
+    if(operation === 'read' &&
+      permissions.indexOf('read') === -1) {
+        reject('user does not have read permissions on this object')
+    }
+    if(operation === 'write' || 'update' || 'destroy' &&
+      permissions.indexOf('write') === -1) {
+        reject('user does not have write permissions on this object')
+    }
+
+    //if it gets this far it's succeeded!
+    //return the parent path and remaining path
+    resolve(
+      {
+          lastParent: lastParent,
+          remainingPath: pathArray.slice(i + 1, pathArray.length)
+      }
+    )
+});
+
+
 module.exports.search = function search( pathObj ) {
     // TODO: hit mongo and search
 
--- src/schema.js
+++ src/schema.js
@@ -17,7 +17,7 @@ var fileSchema = new Schema({
   owner: Objectid,
   permissions: [{
     userId: Objectid,
-    permissions: [String]
+    permissions: [String] //read, write
   }]
 });
 
@@ -53,94 +53,3 @@ var Structure = mongoose.model('Structure', structureSchema);
 //move
 //rename
 //delete
-
-//todo: export this correctly
-//todo: lodash or underscore
-
-var verification = new Promise(function (resolve, reject, user, operation, path) {
-  var lastParent;
-  var pathArray;
-  var i;
-  var folderFail;
-  var isParent;
-  var fileExists;
-  var fileId;
-  var permissionsObject;
-  var permissions;
-
-  //josh/test/docs/sext.doc
-
-  //first we need to find out how far down the path currently exists
-
-  //make the path into an array
-  //todo: need to remove the first part if we have an '/' up front
-  pathArray= path.split('/');
-
-  // start pathFind as an empty string
-  lastParent = '';
-
-  //iterate down the path,
-  i = 0;
-  folderFail = false;
-  do {
-    //add the next part of the path
-    lastParent += pathArray[i];
-
-    //store the fileid
-    //i=2, lastParent = josh.children.test.children.docs.fileId
-    fileId = lastParent + '.fileId';
-
-    //see if this is a folder
-    if (!File.find({$and: [{_id: fileId}, {type: 'folder'}]}).fetch()) {
-      folderFail = true;
-    }
-
-    //add '.children'
-    //i=2, lastParent = josh.children.test.children.docs.children
-    lastParent +='.children';
-
-    //run the test
-    isParent= Structure.find({[lastParent]: {$exists: true, $ne: null}}).fetch();
-
-    //if there's another level and this was not a folder, we have a problem
-    if (isParent && folderFail) {
-      //todo: reject that shit
-    }
-
-    //if there is another level, increment
-    if (isParent) i++;
-
-  } while (isParent);
-
-  //determine if the file exists, eg, is an exact match
-  fileExists = i === pathArray.length;
-
-  //if this is not an exact match, and the last path was not a folder, we have a problem
-  if (!fileExists && folderFail) {
-    //todo: reject that shit
-  }
-
-  //get permissions for the user on the last parent
-  permissionsObject = File.find({_id: id}, {permissions: 1});
-  _.forEach(permissionsObject, function() {
-    if (user === permissionsObject.userId) {
-      permissions = permissionsObject.permissions
-    }
-  });
-  //split permissions into an array
-  permissions = permissions.split(',');
-
-  //we now know where our path ends and what our user's permissions are on that end. time to test things
-  //important vars:
-  // fileExists
-  // permissions
-
-  //todo: a file that already exists is required for some operations, and excluded for others
-
-
-  //todo: test permissions against various actions
-
-
-});
-
-//todo: remember to remove the children array if it's empty!
