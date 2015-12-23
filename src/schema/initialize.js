9fc1902f75e9a3904f2960f31b85118a20ea935d
JoshF <josh.f@ttbventures.com>
2015-12-23 12:11:20 -0500

    rename schema folder to schemas

--- src/schema/initialize.js
+++
@@ -1,5 +0,0 @@
-//START MONGOOSE
-var mongoose = require('mongoose');
-
-//todo: this needs to connect to something
-mongoose.connect('mongodb://localhost/test');
---
+++ src/schemas/initialize.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// START MONGOOSE
+const mongoose = require( 'mongoose' );
+
+// todo: this needs to connect to something
+mongoose.connect( 'mongodb://localhost/test' );
