02d5b63bda95983549f49d2f0afff3c3d76a3d29
JoshF <josh.f@ttbventures.com>
2016-01-06 09:17:26 -0500

    update objectid type in schema

--- src/schemas/fileSchema.js
+++ src/schemas/fileSchema.js
@@ -4,8 +4,8 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-    metaDataId: Objectid, // link to METADATA
-    userId: Objectid, // link to User Collection
+    metaDataId: Schema.Types.ObjectId, // link to METADATA
+    userId: Schema.Types.ObjectId, // link to User Collection
     name: String, // if the resource is a folder, it ends in a '/'
     parent: String, // '/top/mid/'
 });
--- src/schemas/permissionSchmea.js
+++ src/schemas/permissionSchmea.js
@@ -5,10 +5,10 @@ const Schema = mongoose.Schema;
 
 const permissionsSchema = new Schema({
     resourceType: String, // project or file/folder and we can easily add additional resource types later
-    resourceId: Objectid, // links to metadata id or project id
+    resourceId: Schema.Types.ObjectId, // links to metadata id or project id
     appliesTo: String, // 'user', 'group', 'public'
-    userId: Objectid, // if applies to user
-    groupId: Objectid, // if applies to group
+    userId: Schema.Types.ObjectId, // if applies to user
+    groupId: Schema.Types.ObjectId, // if applies to group
     read: Boolean,
     write: Boolean,
     destroy: Boolean,
