d3bf5c8e1a67653a836ace4e5a01057a662aad32
JoshF <josh.f@ttbventures.com>
2016-01-26 20:42:17 -0500

    delete initialize.js. instead call permissions.connect

--- src/schemas/initialize.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// START MONGOOSE
-const mongoose = require( 'mongoose' );
-
-// todo: this needs to connect to something
-// todo: differentiate database based on production vs staging
-mongoose.connect( 'mongodb://localhost/test' );
