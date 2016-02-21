069429100095a2bb18720eddbbabe5030fc449f0
JoshF <josh.f@ttbventures.com>
2016-02-21 13:51:57 -0500

    moving path-checking functions into alias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /* eslint no-unused-vars: 0 */
+/* eslint no-else-return: 0 */
 
 const mongo = require( './mongo.js' );
 const s3 = require( './s3.js' );
@@ -12,6 +13,57 @@ const connect = brinkbitPermissions.connect;
 
 module.exports.schema = {
     file: File,
+};
+
+module.exports.alias = function alias( fullPath, operation ) {
+    let lastGuid;
+    const promises = [];
+    connect
+    .then(() => {
+        // split the path
+        const pathArray = fullPath.split( '/' );
+        pathArray.forEach(( value, index, array ) => {
+            const currentPath = array.slice( 0, index ).join( '/' );
+            // determine that the path exists, has the correct parent, and is a folder
+            File.findOne({ $and: [{ name: currentPath }, { parents: lastGuid }, { mimeType: 'folder' }] }).exec()
+                .then(( file ) => {
+                    // if there is no resource, and this is not the end of the path, we have a problem
+                    if ( !file && index < array.length ) {
+                        return Promise.reject( 'RESOURCE_NOT_FOUND' );
+                    }
+                    // if this is the end of the path, and the resource does not exist, that's a problem for read and modify operations
+                    else if ( !file && index < array.length
+                        && (
+                            operation === 'read' ||
+                            operation === 'update' ||
+                            operation === 'destroy'
+                        )) {
+                        return Promise.reject( 'RESOURCE_NOT_FOUND' );
+                    }
+                    // if this is the end of the path, and the resource does exist, that's a problem for write operations
+                    else if ( file && index < array.length && operation === 'write' ) {
+                        return Promise.reject( 'RESOURCE_EXISTS' );
+                    }
+                    // if there is a file
+                    else {
+                        // store the guid
+                        lastGuid = file._id;
+                        // and send it to the map
+                        promises.push( verify( lastGuid, operation ));
+                    }
+                });
+            // todo: need to do a promise.all of guids into verify, and .then won't work in a map will it?
+        });
+    })
+    .then(() => {
+        Promise.all( promises );
+    })
+    .then(() => {
+        Promise.resolve( lastGuid );
+    })
+    .catch(( e ) => {
+        Promise.reject( e );
+    });
 };
 
 module.exports.read = function read( path ) {
